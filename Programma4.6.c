04aa67d7a8f941402d89901fc7db52162082dda0
Alle Cross Costi <costialle2@hotmail.it>
2018-02-03 15:41:06 +0100

    Initial commit

---
+++ Programma4.6.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main () {
+
+// Questo e un commento su riga singola 
+
+/* Questo e un commento
+* su piu righe
+*nel caso una sola non
+*fosse insufficente */
+
+return 0;
+
+}
